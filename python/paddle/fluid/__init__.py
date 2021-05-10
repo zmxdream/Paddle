dca05df45c5cd5999bbded20edc84cd01905bcdb
humingqing <humingqing@Macintoshs-MacBook-Pro.local>
2021-05-10 17:51:21 +0800

    export FLAGS_enable_binding_train_cpu

--- python/paddle/fluid/__init__.py
+++ python/paddle/fluid/__init__.py
@@ -260,6 +260,7 @@ def __bootstrap__():
             'padbox_slotrecord_extend_dim',
             'padbox_auc_runner_mode',
             'padbox_dataset_enable_unrollinstance',
+            'enable_binding_train_cpu',
         ]
     core.init_gflags(["--tryfromenv=" + ",".join(read_env_flags)])
     core.init_glog(sys.argv[0])
