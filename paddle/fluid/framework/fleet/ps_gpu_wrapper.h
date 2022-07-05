4fa0bab0667c3feff20757c2f9b92e683455f353
zmxdream <zhangminxu01@baidu.com>
2022-07-05 12:55:55 +0800

    revert async build pull

--- paddle/fluid/framework/fleet/ps_gpu_wrapper.h
+++ paddle/fluid/framework/fleet/ps_gpu_wrapper.h
@@ -450,8 +450,8 @@ class PSGPUWrapper {
   std::vector<std::vector<robin_hood::unordered_set<uint64_t>>> thread_keys_;
   std::vector<std::vector<std::vector<robin_hood::unordered_set<uint64_t>>>>
       thread_dim_keys_;
-  int thread_keys_thread_num_ = 37;
-  int thread_keys_shard_num_ = 37;
+  int thread_keys_thread_num_ = 37 * 4;
+  int thread_keys_shard_num_ = 64;
   uint64_t max_fea_num_per_pass_ = 5000000000;
   int year_;
   int month_;
