15f467202abd52e487dcc2af32ef5d383a8b2555
zmxdream <zhangminxu01@baidu.com>
2022-11-23 20:41:08 +0800

    print_table_stat适配多机

--- python/paddle/fluid/incubate/fleet/parameter_server/pslib/__init__.py
+++ python/paddle/fluid/incubate/fleet/parameter_server/pslib/__init__.py
@@ -332,8 +332,8 @@ class PSLib(Fleet):
               fleet.print_table_stat(0)
         """
         self._role_maker._barrier_worker()
-        if self._role_maker.is_first_worker():
-            self._fleet_ptr.print_table_stat(table_id, pass_id, threshold)
+        #if self._role_maker.is_first_worker():
+        self._fleet_ptr.print_table_stat(table_id, pass_id, threshold)
         self._role_maker._barrier_worker()
 
     def set_file_num_one_shard(self, table_id, file_num):
@@ -347,8 +347,8 @@ class PSLib(Fleet):
               fleet.set_file_num_one_shard(0, 5)
         """
         self._role_maker._barrier_worker()
-        if self._role_maker.is_first_worker():
-            self._fleet_ptr.set_file_num_one_shard(table_id, file_num)
+        #if self._role_maker.is_first_worker():
+        self._fleet_ptr.set_file_num_one_shard(table_id, file_num)
         self._role_maker._barrier_worker()
 
     def save_persistables(self, executor, dirname, main_program=None, **kwargs):
