a188f0f625839b508f6956ff9b2064e675b44e94
humingqing <humingqing@Macintoshs-MacBook-Pro.local>
2023-01-05 19:40:51 +0800

    add alias_method_op, add device memory print info

--- paddle/fluid/pybind/box_helper_py.cc
+++ paddle/fluid/pybind/box_helper_py.cc
@@ -114,6 +114,8 @@ void BindBoxWrapper(py::module* m) {
       .def("shrink_resource", &framework::BoxWrapper::ShrinkResource,
            py::call_guard<py::gil_scoped_release>())
       .def("merge_model", &framework::BoxWrapper::MergeModel,
+           py::call_guard<py::gil_scoped_release>())
+      .def("print_device_info", &framework::BoxWrapper::PrintDeviceInfo,
            py::call_guard<py::gil_scoped_release>());
 }  // end BoxWrapper
 void BindBoxFileMgr(py::module* m) {
