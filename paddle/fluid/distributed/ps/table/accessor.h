87c0c6fa2f0b7f0933bbd940184336ea09d2db0d
zmxdream <zhangminxu01@baidu.com>
2023-08-04 11:21:54 +0800

    save non 9008' feasign (#337)
    * save non 9008' feasign
    
    * update
    
    * fix
    
    * fix
    
    * fix
    
    * fix code style
    
    * fix code style

--- paddle/fluid/distributed/ps/table/accessor.h
+++ paddle/fluid/distributed/ps/table/accessor.h
@@ -21,6 +21,7 @@
 
 #include "paddle/fluid/distributed/common/afs_warpper.h"
 #include "paddle/fluid/distributed/common/registerer.h"
+#include "paddle/fluid/distributed/ps/thirdparty/round_robin.h"
 #include "paddle/fluid/distributed/the_one_ps.pb.h"
 
 namespace paddle {
@@ -123,6 +124,7 @@ class ValueAccessor {
   virtual bool SaveSSD(float* value) = 0;
   // 判断热启时是否过滤slot对应的feasign
   virtual bool FilterSlot(float* value) { return false; }
+  virtual bool SaveFilterSlot(float* value) { return false; }
 
   //
   virtual bool SaveCache(float* value,
@@ -173,8 +175,13 @@ class ValueAccessor {
   }
 
   virtual void UpdatePassId(float* value, uint16_t pass_id) {}
-
   virtual float GetField(float* value, const std::string& name) { return 0.0; }
+  virtual robin_hood::unordered_set<float>* GetFilteredSlots() {
+    return nullptr;
+  }
+  virtual robin_hood::unordered_set<float>* GetSaveFilteredSlots() {
+    return nullptr;
+  }
 #define DEFINE_GET_INDEX(class, field) \
   virtual int get_##field##_index() { return class ::field##_index(); }
 
